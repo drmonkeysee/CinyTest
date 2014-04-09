77e256c59017856d71d7ac14fdd7f80e77bd35b2
Brandon Stansbury <brandonrstansbury@gmail.com>
2014-04-08 20:31:11 -0700

    add ciny header

---
+++ CinyTest/Ciny.h
@@ -0,0 +1,14 @@
+//
+//  Ciny.h
+//  CinyTest
+//
+//  Created by Brandon Stansbury on 4/8/14.
+//  Copyright (c) 2014 Monkey Bits. All rights reserved.
+//
+
+#ifndef CinyTest_Ciny_h
+#define CinyTest_Ciny_h
+
+
+
+#endif
