610e1bb271e33cde89acbea0259b07afee58b529
Brandon Stansbury <brandonrstansbury@gmail.com>
2014-04-08 20:42:02 -0700

    re-add source files as lowercase

---
+++ CinyTest/ciny.c
@@ -0,0 +1,9 @@
+//
+//  ciny.c
+//  CinyTest
+//
+//  Created by Brandon Stansbury on 4/8/14.
+//  Copyright (c) 2014 Monkey Bits. All rights reserved.
+//
+
+#include "ciny.h"
---
+++ CinyTest/ciny.h
@@ -0,0 +1,14 @@
+//
+//  ciny.h
+//  CinyTest
+//
+//  Created by Brandon Stansbury on 4/8/14.
+//  Copyright (c) 2014 Monkey Bits. All rights reserved.
+//
+
+#ifndef CinyTest_ciny_h
+#define CinyTest_ciny_h
+
+
+
+#endif
