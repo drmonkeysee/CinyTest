57af5488e0d46965ee5c9142642ee97f175c38d1
Brandon Stansbury <brandonrstansbury@gmail.com>
2014-04-29 16:11:42 -0700

    factor out header and footer output and modify time output to something more formal looking

--- CinyTest/ciny.c
+++ CinyTest/ciny.c
@@ -11,20 +11,39 @@
 #include <stdio.h>
 #include "ciny.h"
 
+#define DATETIME_FORMAT_LENGTH 30
+static const char * const DateFormatString = "%F %T";
+
 // call sites for inline functions
 extern inline struct ct_testcase ct_maketest_full(const char *, ct_test_function);
 extern inline struct ct_testsuite ct_makesuite_full(const char *, struct ct_testcase[], size_t, ct_setupteardown_function, ct_setupteardown_function);
 
+static void print_suiteheader(const struct ct_testsuite *suite, const time_t *start_time)
+{
+    char formatted_datetime[DATETIME_FORMAT_LENGTH];
+    strftime(formatted_datetime, DATETIME_FORMAT_LENGTH, DateFormatString, localtime(start_time));
+    printf("Starting test suite '%s' at %s\n", suite->name, formatted_datetime);
+    
+    printf("Running %zu tests:\n", suite->count);
+}
+
+static void print_suitefooter(const struct ct_testsuite *suite, const time_t * restrict start_time, const time_t * restrict end_time)
+{
+    char formatted_datetime[DATETIME_FORMAT_LENGTH];
+    strftime(formatted_datetime, DATETIME_FORMAT_LENGTH, DateFormatString, localtime(end_time));
+    printf("Test suite '%s' completed at %s\n", suite->name, formatted_datetime);
+    
+    double elapsed_time = difftime(*start_time, *end_time);
+    printf("Ran %zu tests (%.3f seconds): %zu passed, %zu failed, %zu ignored.\n", suite->count, elapsed_time, 0lu, 0lu, 0lu);
+}
+
 size_t ct_runsuite(struct ct_testsuite suite)
 {
     time_t start_time = time(NULL);
-    printf("Starting test suite '%s' at %s", suite.name, ctime(&start_time));
-    printf("Running %zu tests:\n", suite.count);
+    print_suiteheader(&suite, &start_time);
     
     time_t end_time = time(NULL);
-    double elapsed_time = difftime(start_time, end_time);
-    printf("Test suite '%s' completed at %s", suite.name, ctime(&end_time));
-    printf("Ran %zu tests (%.3f seconds): %zu passed, %zu failed, %zu ignored.\n", suite.count, elapsed_time, 0lu, 0lu, 0lu);
+    print_suitefooter(&suite, &start_time, &end_time);
     
     return 0;
 }
