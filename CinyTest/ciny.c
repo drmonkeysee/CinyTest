f0f2ea414910c10efa80ed2a080c4ee87752c314
Brandon Stansbury <brandonrstansbury@gmail.com>
2014-04-29 14:48:45 -0700

    stub out runsuite and test class

--- CinyTest/ciny.c
+++ CinyTest/ciny.c
@@ -12,3 +12,8 @@
 // call sites for inline functions
 extern inline struct ct_testcase ct_maketest_full(const char *, ct_test_function);
 extern inline struct ct_testsuite ct_makesuite_full(const char *, struct ct_testcase[], size_t, ct_setupteardown_function, ct_setupteardown_function);
+
+size_t ct_runsuite(struct ct_testsuite suite)
+{
+    return 0;
+}
---
+++ CinyTestTests/CTRunSuiteTests.m
@@ -0,0 +1,29 @@
+//
+//  CTRunSuiteTests.m
+//  CinyTest
+//
+//  Created by Brandon Stansbury on 4/29/14.
+//  Copyright (c) 2014 Monkey Bits. All rights reserved.
+//
+
+#import "ciny.h"
+
+@interface CTRunSuiteTests : XCTestCase
+
+@end
+
+@implementation CTRunSuiteTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+@end
