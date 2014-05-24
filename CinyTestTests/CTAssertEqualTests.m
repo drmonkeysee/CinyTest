23fa20e481c239fb90186aaeee5d72b657b82580
Brandon Stansbury <brandonrstansbury@gmail.com>
2014-05-24 15:45:58 -0700

    add assert equal tests file

---
+++ CinyTestTests/CTAssertEqualTests.m
@@ -0,0 +1,34 @@
+//
+//  CTAssertEqualTests.m
+//  CinyTest
+//
+//  Created by Brandon Stansbury on 5/24/14.
+//  Copyright (c) 2014 Brandon Stansbury. All rights reserved.
+//
+
+#include "ciny.h"
+
+@interface CTAssertEqualTests : XCTestCase
+
+@end
+
+@implementation CTAssertEqualTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
