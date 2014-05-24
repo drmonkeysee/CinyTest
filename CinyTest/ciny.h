db036a9eb8f015d95d3a4a17423a83b57e27d740
Brandon Stansbury <brandonrstansbury@gmail.com>
2014-05-24 15:52:20 -0700

    shorten static assert invalid value message

--- CinyTest/ciny.h
+++ CinyTest/ciny.h
@@ -267,7 +267,7 @@ struct ct_comparable_value {
                                         double _Complex: CT_ANNOTATE_COMPLEX, \
                                         long double _Complex: CT_ANNOTATE_COMPLEX, \
                                         default: CT_ANNOTATE_INVALID)
-#define ct_checkvalue(v) _Static_assert(ct_valuetype_annotation(v), "invalid value type; use ct_assertequalp for pointer types, ct_assertequalstr for string types, or custom code with ct_asserttrue/ct_assertfalse for structs, unions, and arrays.")
+#define ct_checkvalue(v) _Static_assert(ct_valuetype_annotation(v), "invalid value type; use ct_assertequalp for pointer types, ct_assertequalstr for string types, or custom comparisons with ct_asserttrue for structs, unions, and arrays.")
 #define ct_makevalue(v, T) (struct ct_comparable_value){ v, T }
 
 /**
