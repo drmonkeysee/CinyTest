7a5fb668ed64921baa099a1424076dafda271e3a
Brandon Stansbury <brandonrstansbury@gmail.com>
2014-06-28 19:27:07 -0700

    first set of basic tests

--- CinyTestTests/CTAssertAboutEqualTests.m
+++ CinyTestTests/CTAssertAboutEqualTests.m
@@ -67,9 +67,148 @@ static void about_equality_test(void *context)
     [super tearDown];
 }
 
-- (void)testExample
+- (void)test_ctaboutequal_ComparesEqual_IfEqualValues
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    d_values[ARG_EXPECTED] = 4.5;
+    d_values[ARG_ACTUAL] = 4.5;
+    d_values[ARG_PRECISION] = 0.0;
+    self.expectedType = TAT_DOUBLE;
+    self.actualType = TAT_DOUBLE;
+    self.precisionType = TAT_DOUBLE;
+    struct ct_testcase tests[] = { ct_maketest(about_equality_test) };
+    struct ct_testsuite suite = ct_makesuite(tests);
+    
+    size_t run_result = ct_runsuite(&suite);
+    
+    XCTAssertEqual(0, run_result);
+    XCTAssertTrue(self.invokedTest);
+    XCTAssertTrue(self.sawPostAssertCode);
+}
+
+- (void)test_ctaboutequal_ComparesEqual_IfWithinPrecision
+{
+    d_values[ARG_EXPECTED] = 4.5;
+    d_values[ARG_ACTUAL] = 4.3;
+    d_values[ARG_PRECISION] = 0.5;
+    self.expectedType = TAT_DOUBLE;
+    self.actualType = TAT_DOUBLE;
+    self.precisionType = TAT_DOUBLE;
+    struct ct_testcase tests[] = { ct_maketest(about_equality_test) };
+    struct ct_testsuite suite = ct_makesuite(tests);
+    
+    size_t run_result = ct_runsuite(&suite);
+    
+    XCTAssertEqual(0, run_result);
+    XCTAssertTrue(self.invokedTest);
+    XCTAssertTrue(self.sawPostAssertCode);
+}
+
+- (void)test_ctaboutequal_ComparesEqual_IfExactlyOnPrecision
+{
+    d_values[ARG_EXPECTED] = 4.5;
+    d_values[ARG_ACTUAL] = 4.51;
+    d_values[ARG_PRECISION] = 0.01;
+    self.expectedType = TAT_DOUBLE;
+    self.actualType = TAT_DOUBLE;
+    self.precisionType = TAT_DOUBLE;
+    struct ct_testcase tests[] = { ct_maketest(about_equality_test) };
+    struct ct_testsuite suite = ct_makesuite(tests);
+    
+    size_t run_result = ct_runsuite(&suite);
+    
+    XCTAssertEqual(0, run_result);
+    XCTAssertTrue(self.invokedTest);
+    XCTAssertTrue(self.sawPostAssertCode);
+}
+
+- (void)test_ctaboutequal_ComparesEqual_IfNegativePrecision
+{
+    d_values[ARG_EXPECTED] = 4.5;
+    d_values[ARG_ACTUAL] = 4.3;
+    d_values[ARG_PRECISION] = -0.5;
+    self.expectedType = TAT_DOUBLE;
+    self.actualType = TAT_DOUBLE;
+    self.precisionType = TAT_DOUBLE;
+    struct ct_testcase tests[] = { ct_maketest(about_equality_test) };
+    struct ct_testsuite suite = ct_makesuite(tests);
+    
+    size_t run_result = ct_runsuite(&suite);
+    
+    XCTAssertEqual(0, run_result);
+    XCTAssertTrue(self.invokedTest);
+    XCTAssertTrue(self.sawPostAssertCode);
+}
+
+- (void)test_ctaboutequal_ComparesEqual_IfPrecisionIsGreaterThanValues
+{
+    d_values[ARG_EXPECTED] = 1.5;
+    d_values[ARG_ACTUAL] = 2.3;
+    d_values[ARG_PRECISION] = 3.0;
+    self.expectedType = TAT_DOUBLE;
+    self.actualType = TAT_DOUBLE;
+    self.precisionType = TAT_DOUBLE;
+    struct ct_testcase tests[] = { ct_maketest(about_equality_test) };
+    struct ct_testsuite suite = ct_makesuite(tests);
+    
+    size_t run_result = ct_runsuite(&suite);
+    
+    XCTAssertEqual(0, run_result);
+    XCTAssertTrue(self.invokedTest);
+    XCTAssertTrue(self.sawPostAssertCode);
+}
+
+- (void)test_ctaboutequal_ComparesNotEqual_IfDifferentWidthsAndZeroPrecision
+{
+    f_values[ARG_EXPECTED] = 22.0f / 7.0f;
+    d_values[ARG_ACTUAL] = 22.0 / 7.0;
+    d_values[ARG_PRECISION] = 0.0;
+    self.expectedType = TAT_FLOAT;
+    self.actualType = TAT_DOUBLE;
+    self.precisionType = TAT_DOUBLE;
+    struct ct_testcase tests[] = { ct_maketest(about_equality_test) };
+    struct ct_testsuite suite = ct_makesuite(tests);
+    
+    size_t run_result = ct_runsuite(&suite);
+    
+    XCTAssertEqual(1, run_result);
+    XCTAssertTrue(self.invokedTest);
+    XCTAssertFalse(self.sawPostAssertCode);
+}
+
+- (void)test_ctaboutequal_ComparesNotEqual_IfOutsideOfPrecision
+{
+    d_values[ARG_EXPECTED] = 5.2;
+    d_values[ARG_ACTUAL] = 5.9;
+    d_values[ARG_PRECISION] = 0.3;
+    self.expectedType = TAT_FLOAT;
+    self.actualType = TAT_DOUBLE;
+    self.precisionType = TAT_DOUBLE;
+    struct ct_testcase tests[] = { ct_maketest(about_equality_test) };
+    struct ct_testsuite suite = ct_makesuite(tests);
+    
+    size_t run_result = ct_runsuite(&suite);
+    
+    XCTAssertEqual(1, run_result);
+    XCTAssertTrue(self.invokedTest);
+    XCTAssertFalse(self.sawPostAssertCode);
+}
+
+- (void)test_ctaboutequal_ComparesNotEqual_IfJustBeyondPrecision
+{
+    d_values[ARG_EXPECTED] = 5.4;
+    d_values[ARG_ACTUAL] = 5.5001;
+    d_values[ARG_PRECISION] = 0.1;
+    self.expectedType = TAT_FLOAT;
+    self.actualType = TAT_DOUBLE;
+    self.precisionType = TAT_DOUBLE;
+    struct ct_testcase tests[] = { ct_maketest(about_equality_test) };
+    struct ct_testsuite suite = ct_makesuite(tests);
+    
+    size_t run_result = ct_runsuite(&suite);
+    
+    XCTAssertEqual(1, run_result);
+    XCTAssertTrue(self.invokedTest);
+    XCTAssertFalse(self.sawPostAssertCode);
 }
 
 @end
