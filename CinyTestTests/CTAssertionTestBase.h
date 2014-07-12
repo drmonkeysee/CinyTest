13622c2301c093cfbb0931fbdfd9b95084769b49
Brandon Stansbury <brandonrstansbury@gmail.com>
2014-07-11 20:33:37 -0700

    add assertion testcase base

---
+++ CinyTestTests/CTAssertionTestBase.h
@@ -0,0 +1,14 @@
+//
+//  CTAssertionTestBase.h
+//  CinyTest
+//
+//  Created by Brandon Stansbury on 7/11/14.
+//  Copyright (c) 2014 Brandon Stansbury. All rights reserved.
+//
+
+@interface CTAssertionTestBase : XCTestCase
+
+@property (nonatomic, assign) BOOL invokedTest;
+@property (nonatomic, assign) BOOL sawPostAssertCode;
+
+@end
---
+++ CinyTestTests/CTAssertionTestBase.m
@@ -0,0 +1,13 @@
+//
+//  CTAssertionTestBase.m
+//  CinyTest
+//
+//  Created by Brandon Stansbury on 7/11/14.
+//  Copyright (c) 2014 Brandon Stansbury. All rights reserved.
+//
+
+#import "CTAssertionTestBase.h"
+
+@implementation CTAssertionTestBase
+
+@end
