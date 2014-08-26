172fd7dc20199a8db19b7a293ae005547cb07275
Brandon Stansbury <brandonrstansbury@gmail.com>
2014-08-25 19:56:09 -0700

    give main something to do in the sample project

--- CinyTest-Sample/CinyTest-Sample/main.c
+++ CinyTest-Sample/CinyTest-Sample/main.c
@@ -7,12 +7,19 @@
 //
 
 #include <stdio.h>
+#include "binarytree.h"
 
 int main(int argc, const char *argv[])
 {
+    binarytree *tree = bt_createwithvalues(12, 1, 2, 3, 6, 5, 4, 10, 11, 12, 13, 14, 15);
+    printf("This tree is wack:\n");
+    bt_print(tree);
+    
+    bt_rebalance(&tree);
+    printf("This tree has been dewackified:\n");
+    bt_print(tree);
 
-    // insert code here...
-    printf("Hello, World!\n");
+    bt_free(tree);
+    
     return 0;
 }
-
