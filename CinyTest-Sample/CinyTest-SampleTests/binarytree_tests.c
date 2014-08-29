90b86ca9a37c609eed2ff4dc28271b630cbee5cf
Brandon Stansbury <brandonrstansbury@gmail.com>
2014-08-29 12:08:07 -0700

    wire up basic binarytree test

--- CinyTest-Sample/CinyTest-SampleTests/binarytree_tests.c
+++ CinyTest-Sample/CinyTest-SampleTests/binarytree_tests.c
@@ -7,8 +7,35 @@
 //
 
 #include <cinytest/ciny.h>
+#include "binarytree.h"
+
+static void setup(void **context)
+{
+    *context = bt_create();
+}
+
+static void teardown(void **context)
+{
+    bt_free(*context);
+}
+
+static void btcreate_creates_emptytree(void *context)
+{
+    binarytree *tree = context;
+    
+    _Bool empty = bt_isempty(tree);
+    
+    ct_asserttrue(empty);
+}
 
 size_t binarytree_tests(void)
 {
-    return 1;
+    struct ct_testcase tests[] = {
+        ct_maketest(btcreate_creates_emptytree)
+    };
+    struct ct_testsuite suite = ct_makesuite_setup_teardown(tests, setup, teardown);
+    
+    size_t results = ct_runsuite(&suite);
+    
+    return results;
 }
