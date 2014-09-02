76e51cec801c0d5d7e512baf697e7f0a75b556a0
Brandon Stansbury <brandonrstansbury@gmail.com>
2014-09-01 21:03:31 -0700

    force milliseconds to 64-bit width and rework arithemetic to reduce risk of overflow

--- CinyTest/CinyTest/ciny_posix.c
+++ CinyTest/CinyTest/ciny_posix.c
@@ -6,15 +6,17 @@
 //  Copyright (c) 2014 Brandon Stansbury. All rights reserved.
 //
 
+#include <stdint.h>
 #include <time.h>
 #include <sys/time.h>
 
-static const long NanosecondsPerMicrosecond = 1000;
-static const long NanosecondsPerMillisecond = 1000000;
+static const uint64_t NanosecondsPerMicrosecond = 1000;
+static const uint64_t MillisecondsPerSecond = 1000;
+static const uint64_t NanosecondsPerMillisecond = 1000000;
 
 // OS X has not implemented timespec_get() yet so
 // use gettimeofday() for POSIX-compliant builds.
-long get_currentmsecs(void)
+uint64_t get_currentmsecs(void)
 {
     struct timeval vtime;
     if (gettimeofday(&vtime, NULL) != 0) {
@@ -23,5 +25,5 @@ long get_currentmsecs(void)
     
     // proxy through the C11 timespec type so returned value is standards compliant
     struct timespec time = { vtime.tv_sec, vtime.tv_usec * NanosecondsPerMicrosecond };
-    return time.tv_nsec / NanosecondsPerMillisecond;
+    return (time.tv_sec * MillisecondsPerSecond) + (time.tv_nsec / NanosecondsPerMillisecond);
 }
