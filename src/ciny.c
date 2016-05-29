73fe39f9863774d1613d73ec6071a7253335cd42
Brandon Stansbury <brandonrstansbury@gmail.com>
2016-05-28 19:32:27 -0700

    make global const local

--- src/ciny.c
+++ src/ciny.c
@@ -32,7 +32,6 @@ uint64_t get_currentmsecs(void);
 static const char * const restrict DateFormatString = "%F %T";
 static const char * const restrict InvalidDateFormat = "Invalid Date (formatted output may have exceeded buffer size)";
 static const char IgnoredTestGlyph = '?';
-static const double MillisecondsPerSecond = 1000.0;
 
 #define COMPVALUE_STR_SIZE 75
 enum assert_type {
@@ -91,11 +90,12 @@ static void print_runheader(const struct ct_testsuite *suite, time_t start_time)
 
 static void print_runfooter(const struct ct_testsuite *suite, time_t end_time, uint64_t elapsed_msecs, const struct runledger *ledger)
 {
+    static const double ms_per_sec = 1000.0;
     char formatted_datetime[DATE_FORMAT_SIZE];
     size_t format_length = strftime(formatted_datetime, sizeof formatted_datetime, DateFormatString, localtime(&end_time));
     printf("Test suite '%s' completed at %s\n", suite->name, format_length ? formatted_datetime : InvalidDateFormat);
     
-    printf("Ran %zu tests (%.3f seconds): %zu passed, %zu failed, %zu ignored.\n", suite->count, elapsed_msecs / MillisecondsPerSecond, ledger->passed, ledger->failed, ledger->ignored);
+    printf("Ran %zu tests (%.3f seconds): %zu passed, %zu failed, %zu ignored.\n", suite->count, elapsed_msecs / ms_per_sec, ledger->passed, ledger->failed, ledger->ignored);
     
     print_delimiter("End");
 }
