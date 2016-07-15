9403249c3513d80633da6ab2714c0c5d9eff82a9
Brandon Stansbury <brandonrstansbury@gmail.com>
2016-07-14 20:07:14 -0700

    bump ct version

--- src/ciny.h
+++ src/ciny.h
@@ -28,7 +28,7 @@
 /**
  Current version of CinyTest as MAJOR.MINOR.PATCH string (using semantic versioning).
  */
-#define CT_VERSION "1.2.1"
+#define CT_VERSION "1.3.0"
 
 /**
  Type definition for a unit test function.
