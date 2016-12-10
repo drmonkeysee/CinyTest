6d09965608ff3c28510f3072c7b1eb5c3e0d3853
Brandon Stansbury <brandonrstansbury@gmail.com>
2016-12-09 19:08:15 -0800

    add anticipated command line arg tests (and make branch that should have been created several commits ago)

--- mac/Tests/CTColorOptionTests.m
+++ mac/Tests/CTColorOptionTests.m
@@ -12,6 +12,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 #include "ciny.h"
 
 @interface CTColorOptionTests : XCTestCase
@@ -41,28 +42,61 @@ static void test_case(void *context)
 
 - (void)test_colorizedEnabledByDefault
 {
-    [self assertSuiteOutputContains:@"[0;32m1 passed[0m," for:nil];
+    [self assertSuiteOutputContains:@"[0;32m1 passed[0m," forValue:nil];
 }
 
 - (void)test_colorizedDisabledByEnv
 {
-    NSArray *disableValues = [NSArray arrayWithObjects:@"NO", @"no", @"FALSE", @"false", @"0", @"n", @"F", nil];
+    NSArray *disableValues = @[@"NO", @"no", @"FALSE", @"false", @"0", @"n", @"F"];
     for (NSString *value in disableValues) {
         setenv("CINYTEST_COLORIZED", value.UTF8String, 1);
-        [self assertSuiteOutputContains:@"1 passed," for:value];
+        [self assertSuiteOutputContains:@"1 passed," forValue:value];
     }
 }
 
 - (void)test_colorizedEnabledForPositiveEnvValues
 {
-    NSArray *randomValues = [NSArray arrayWithObjects:@"YES", @"true", @"blarg", @"", @"1", nil];
+    NSArray *randomValues = @[@"YES", @"true", @"blarg", @"", @"1"];
     for (NSString *value in randomValues) {
         setenv("CINYTEST_COLORIZED", value.UTF8String, 1);
-        [self assertSuiteOutputContains:@"[0;32m1 passed[0m," for:value];
+        [self assertSuiteOutputContains:@"[0;32m1 passed[0m," forValue:value];
     }
 }
 
-- (void)assertSuiteOutputContains:(NSString *)expected for:(NSString *)envValue
+- (void)test_colorizedEnabledWithArbitraryArgs
+{
+    NSArray *args = @[@"my-program", @"--foo=1", @"-v", @"--ct-somethingelse=NO"];
+    [self assertSuiteOutputContains:@"[0;32m1 passed[0m," forValue:nil withArgs:args];
+}
+
+- (void)test_colorizedDisabledWithCommandLineArg
+{
+    NSArray *disableValues = @[@"--ct-colorized=NO", @"--ct-colorized=no", @"--ct-colorized=FALSE", @"--ct-colorized=false", @"--ct-colorized=0", @"--ct-colorized=N", @"--ct-colorized=f"];
+    for (NSString *value in disableValues) {
+        NSMutableArray *args = [NSMutableArray arrayWithObjects:@"my-program", @"--foo=1", @"-v", @"--ct-somethingelse=NO", nil];
+        uint32_t insertIndex = arc4random_uniform((uint32_t)args.count);
+        [args insertObject:value atIndex:insertIndex];
+        [self assertSuiteOutputContains:@"1 passed," forValue:value withArgs:args];
+    }
+}
+
+- (void)test_colorizedEnabledWithPositiveCommandLineArg
+{
+    NSArray *randomValues = @[@"--ct-colorized=YES", @"--ct-colorized=true", @"--ct-colorized=blarg", @"--ct-colorized=", @"--ct-colorized", @"--ct-colorized=1"];
+    for (NSString *value in randomValues) {
+        NSMutableArray *args = [NSMutableArray arrayWithObjects:@"my-program", @"--foo=1", @"-v", @"--ct-somethingelse=NO", nil];
+        uint32_t insertIndex = arc4random_uniform((uint32_t)args.count);
+        [args insertObject:value atIndex:insertIndex];
+        [self assertSuiteOutputContains:@"[0;32m1 passed[0m," forValue:value withArgs:args];
+    }
+}
+
+- (void)assertSuiteOutputContains:(NSString *)expected forValue:(NSString *)colorFlag
+{
+    [self assertSuiteOutputContains:expected forValue:colorFlag withArgs:[NSArray array]];
+}
+
+- (void)assertSuiteOutputContains:(NSString *)expected forValue:(NSString *)colorFlag withArgs:(NSArray *)args
 {
     const struct ct_testcase cases[] = { ct_maketest(test_case) };
     struct ct_testsuite suite = ct_makesuite(cases);
@@ -73,6 +107,7 @@ static void test_case(void *context)
     dup2(output.fileHandleForWriting.fileDescriptor, fileno(stdout));
     
     ct_runsuite(&suite);
+    // ct_runsuite_withargs(&suite, argc, argv);
     
     fflush(stdout);
     NSData *data = readOutput.availableData;
@@ -82,8 +117,8 @@ static void test_case(void *context)
     NSString *result = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
     NSRange found = [result rangeOfString:expected];
     
-    XCTAssertNotNil(result, @"Unexpected nil output string for env value \"%@\"", envValue);
-    XCTAssertNotEqual(NSNotFound, found.location, @"Expected string \"%@\" not found for env value \"%@\"", expected, envValue);
+    XCTAssertNotNil(result, @"Unexpected nil output string for color flag value \"%@\"", colorFlag);
+    XCTAssertNotEqual(NSNotFound, found.location, @"Expected string \"%@\" not found for color flag value \"%@\"", expected, colorFlag);
 }
 
 @end
