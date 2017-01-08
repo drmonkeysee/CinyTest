b8a4187b124a779c889b78e52b191faed0e6ce56
Brandon Stansbury <brandonrstansbury@gmail.com>
2017-01-07 22:01:45 -0800

    common test base class for all tests to control environment

---
+++ mac/Tests/CTTestBase.h
@@ -0,0 +1,11 @@
+//
+//  CTTestBase.h
+//  CinyTest
+//
+//  Created by Brandon Stansbury on 1/7/17.
+//  Copyright © 2017 Brandon Stansbury. All rights reserved.
+//
+
+@interface CTTestBase : XCTestCase
+
+@end
---
+++ mac/Tests/CTTestBase.m
@@ -0,0 +1,28 @@
+//
+//  CTTestBase.m
+//  CinyTest
+//
+//  Created by Brandon Stansbury on 1/7/17.
+//  Copyright © 2017 Brandon Stansbury. All rights reserved.
+//
+
+#import "CTTestBase.h"
+#include <stdlib.h>
+
+@implementation CTTestBase
+
+- (void)setUp
+{
+    [super setUp];
+    
+    setenv("CINYTEST_COLORIZED", "NO", 1);
+}
+
+- (void)tearDown
+{
+    unsetenv("CINYTEST_COLORIZED");
+    
+    [super tearDown];
+}
+
+@end
