0e5525bb97bc3a1927e80dffdbf0eb21bca3749b
Brandon Stansbury <brandonrstansbury@gmail.com>
2017-01-22 19:58:42 -0800

    add version option test

---
+++ mac/Tests/CTVersionOptionTests.m
@@ -0,0 +1,39 @@
+//
+//  CTVersionOptionTests.m
+//  CinyTest
+//
+//  Created by Brandon Stansbury on 1/22/17.
+//  Copyright © 2017 Brandon Stansbury. All rights reserved.
+//
+
+#import "CTAssertionTestBase.h"
+#include "ciny.h"
+
+@interface CTVersionOptionTests : CTAssertionTestBase
+
+@end
+
+static void test_case(void *context)
+{
+    CTVersionOptionTests *testObject = (__bridge CTVersionOptionTests *)(TestClass);
+    
+    testObject.invokedTest = YES;
+    
+    ct_asserttrue(true, "true test");
+}
+
+@implementation CTVersionOptionTests
+
+- (void)test_noTestsRun_IfVersionOptionSet
+{
+    const struct ct_testcase cases[] = { ct_maketest(test_case) };
+    const struct ct_testsuite suite = ct_makesuite(cases);
+    const char *args[] = { "--ct-version" };
+    
+    const size_t result = ct_runsuite_withargs(&suite, sizeof args / sizeof args[0], args);
+    
+    XCTAssertEqual(0u, result);
+    XCTAssertFalse(self.invokedTest);
+}
+
+@end
