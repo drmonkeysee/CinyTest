5201a0530e826eaac3b03ddcaa6b294e3bcdd04d
Brandon Stansbury <brandonrstansbury@gmail.com>
2017-01-22 20:16:08 -0800

    rename test class and add help option test

--- mac/Tests/CTNoRunOptionsTests.m
+++ mac/Tests/CTNoRunOptionsTests.m
@@ -1,5 +1,5 @@
 //
-//  CTVersionOptionTests.m
+//  CTNoRunOptionsTests.m
 //  CinyTest
 //
 //  Created by Brandon Stansbury on 1/22/17.
@@ -9,26 +9,38 @@
 #import "CTAssertionTestBase.h"
 #include "ciny.h"
 
-@interface CTVersionOptionTests : CTAssertionTestBase
+@interface CTNoRunOptionsTests : CTAssertionTestBase
 
 @end
 
 static void test_case(void *context)
 {
-    CTVersionOptionTests *testObject = (__bridge CTVersionOptionTests *)(TestClass);
+    CTNoRunOptionsTests *testObject = (__bridge CTNoRunOptionsTests *)(TestClass);
     
     testObject.invokedTest = YES;
     
     ct_asserttrue(true, "true test");
 }
 
-@implementation CTVersionOptionTests
+@implementation CTNoRunOptionsTests
 
 - (void)test_noTestsRun_IfVersionOptionSet
 {
     const struct ct_testcase cases[] = { ct_maketest(test_case) };
     const struct ct_testsuite suite = ct_makesuite(cases);
     const char *args[] = { "--ct-version" };
+    
+    const size_t result = ct_runsuite_withargs(&suite, sizeof args / sizeof args[0], args);
+    
+    XCTAssertEqual(0u, result);
+    XCTAssertFalse(self.invokedTest);
+}
+
+- (void)test_noTestsRun_IfHelpOptionSet
+{
+    const struct ct_testcase cases[] = { ct_maketest(test_case) };
+    const struct ct_testsuite suite = ct_makesuite(cases);
+    const char *args[] = { "--ct-help" };
     
     const size_t result = ct_runsuite_withargs(&suite, sizeof args / sizeof args[0], args);
     
