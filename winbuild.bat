5db3c8a3139c2f02f994bd5bece96c0239e067f7
Brandon Stansbury <brandonrstansbury@gmail.com>
2017-02-21 06:06:04 +0000

    add all build steps to win script

--- winbuild.bat
+++ winbuild.bat
@@ -3,15 +3,45 @@ SETLOCAL
 
 SET build_dir=build
 SET src_dir=src
+SET samp_src_dir=%src_dir%\sample
+SET samp_test_src_dir=test\sample
 SET obj_dir=%build_dir%\obj
+SET lib_dir=%build_dir%\lib
+SET inc_dir=%build_dir%\include
 
 SET cc=clang
-SET cflags=-Wall -Wextra -Wno-deprecated-declarations -pedantic -Werror -std=c11 -Os
+SET cflags=-Wall -Wextra -Wno-deprecated-declarations -pedantic -Werror -std=c11 -Os -v
 
 RMDIR /S /Q %build_dir%
-MKDIR %obj_dir%
 
-%cc% %cflags%  -iquote%src_dir% -c %src_dir%\ciny.c -o %obj_dir%\ciny.o -v
+ECHO:
+ECHO Build CinyTest Library...
+MKDIR %obj_dir%
+%cc% %cflags% -iquote%src_dir% -c %src_dir%\ciny.c -o %obj_dir%\ciny.obj
+IF "%ERRORLEVEL%" NEQ "0" GOTO end
+
+REM %cc% %cflags% -iquote%src_dir% -c %src_dir%\ciny_win.c -o %obj_dir%\ciny_win.obj
+IF "%ERRORLEVEL%" NEQ "0" GOTO end
+
+MKDIR %lib_dir%
+REM LIB %obj_dir%\ciny.obj %obj_dir%\ciny_win.obj /OUT:%lib_dir%\cinytest.lib /VERBOSE /NOLOGO
+IF "%ERRORLEVEL%" NEQ "0" GOTO end
+
+MKDIR %inc_dir%
+COPY %src_dir%\ciny.h %inc_dir%
+IF "%ERRORLEVEL%" NEQ "0" GOTO end
+
+ECHO:
+ECHO Build CinyTest Sample...
+%cc% %cflags% -iquote%samp_src_dir% %samp_src_dir%\binarytree.c %samp_src_dir%\main.c -o %build_dir%\sample.exe
+IF "%ERRORLEVEL%" NEQ "0" GOTO end
+
+ECHO:
+ECHO Build CinyTest Sample Tests...
+TODO: clang knows of _Complex but win complex.h defines funcs based on structs, structs not visible without including header
+%cc% %cflags% -iquote%build_dir%\include -iquote%samp_src_dir% ^
+	-L%lib_dir% -lcinytest %samp_src_dir%\binarytree.c %samp_test_src_dir%\main.c %samp_test_src_dir%\binarytree_tests.c ^
+	-o %build_dir%\sampletests.exe
 IF "%ERRORLEVEL%" NEQ "0" GOTO end
 
 :end
