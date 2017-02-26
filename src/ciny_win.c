4a2463c8c01c0c65b59b81b9a23dbe9af0845902
Brandon Stansbury <brandonrstansbury@gmail.com>
2017-02-25 21:32:55 -0800

    add win-specific src file

---
+++ src/ciny_win.c
@@ -0,0 +1,12 @@
+//
+//  ciny_win.c
+//  CinyTest
+//
+//  Created by Brandon Stansbury on 2/25/17.
+//  Copyright © 2017 Brandon Stansbury. All rights reserved.
+//
+
+uint64_t ct_get_currentmsecs(void)
+{
+    return 12345;
+}
