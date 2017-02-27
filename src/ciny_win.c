72a1c3e3f45f3c3270e55adbe992d6224f9cfa4a
Brandon Stansbury <brandonrstansbury@gmail.com>
2017-02-26 20:09:05 -0800

    get current time in milliseconds from sys time

--- src/ciny_win.c
+++ src/ciny_win.c
@@ -6,13 +6,21 @@
 //  Copyright © 2017 Brandon Stansbury. All rights reserved.
 //
 
+#define WIN32_LEAN_AND_MEAN
 #include <stdint.h>
+#include <Windows.h>
 
-// TODO: get real current time in msecs
 // TODO: print colors properly
 // TODO: print non-unicode for nt shell friendly output
 
+static const uint64_t MillisecondFactor = 10000;
+
 uint64_t ct_get_currentmsecs(void)
 {
-    return 12345;
+    LPFILETIME vtime;
+    GetSystemTimeAsFileTime(&vtime);
+    
+    ULARGE_INTEGER ntime = { .LowPart = vtime.dwLowDateTime, .HighPart = vtime.dwHighDateTime };
+    
+    return ntime.QuadPart / MillisecondFactor;
 }
