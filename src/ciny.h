95095feab3d3d5a20d01ffb1b3b45b41f8e37ff7
Brandon Stansbury <brandonrstansbury@gmail.com>
2017-09-07 22:10:51 -0700

    bump version

--- src/ciny.h
+++ src/ciny.h
@@ -39,7 +39,7 @@ struct ct_version {
  */
 inline struct ct_version ct_getversion(void)
 {
-    return (struct ct_version){ 3, 0, 1 };
+    return (struct ct_version){ 3, 1, 0 };
 }
 /**
  Convert a semantic verson to hexadecimal form for easy numeric comparisons.
