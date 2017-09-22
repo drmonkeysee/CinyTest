2ce80dea49987c0e5ab530795dc1bbacfd2aba6d
Brandon Stansbury <brandonrstansbury@gmail.com>
2017-09-21 22:55:49 -0700

    stub include option tests

---
+++ mac/Tests/CTIncludeOptionTests.m
@@ -0,0 +1,17 @@
+//
+//  CTIncludeOptionTests.m
+//  Tests
+//
+//  Created by Brandon Stansbury on 9/21/17.
+//  Copyright © 2017 Brandon Stansbury. All rights reserved.
+//
+
+#import "CTTestBase.h"
+
+@interface CTIncludeOptionTests : CTTestBase
+
+@end
+
+@implementation CTIncludeOptionTests
+
+@end
